87efecd8a66fa4372772b813a63ede34c04f2fdd
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-03-21 23:59:51 +0000

    add ssql resuqteste to create tables if doesn't exist in dbSingleton

--- src/main/java/com/example/studentapp/DAO/DBSingleton.java
+++ src/main/java/com/example/studentapp/DAO/DBSingleton.java
@@ -14,12 +14,29 @@ public class DBSingleton {
             Class.forName("com.mysql.cj.jdbc.Driver"); // Use "com.mysql.cj.jdbc.Driver" for MySQL 8 and above
             connection = DriverManager.getConnection(config.getUrl(), config.getUser(), config.getPassword());
             config.connectAndCreateIfNotExist();
+            createTables();
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException("Error initializing database connection", e);
         }
     }
 
+    private static void createTables() throws SQLException {
+        Statement stmt = connection.createStatement();
+
+        String sqlStudent = "CREATE TABLE IF NOT EXISTS Student (" +
+                "id INT PRIMARY KEY AUTO_INCREMENT, " +
+                "name VARCHAR(50), " +
+                "age INT, " +
+                "ville_id INT)";
+        stmt.execute(sqlStudent);
+
+        String sqlVille = "CREATE TABLE IF NOT EXISTS Ville (" +
+                "id INT PRIMARY KEY AUTO_INCREMENT, " +
+                "name VARCHAR(50))";
+        stmt.execute(sqlVille);
+    }
     public static Connection getConnection() {
         return connection;
     }
+
 }
